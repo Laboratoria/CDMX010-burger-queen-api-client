f1f92149ea5878ccd57f07730b0254d2eb28710b
MiriamNM <miriamnavamucino@gmail.com>
2021-04-23 11:02:35 -0500

    templates, adding 5000 changes

---
+++ Slice/menu.js
@@ -0,0 +1,16 @@
+const menu = () => {`
+    <form>
+        <div>
+            <input type='name'></input>
+        </div>
+        <div>
+            <input type='checkbox'>
+            <label>Morning</label>
+            <label>All day </label>
+            </input>
+        </div>
+        <div id=order></div>
+        <input></input>
+        <button>SUBMIT</button>
+    </form>
+`}
