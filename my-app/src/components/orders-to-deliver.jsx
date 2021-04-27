5c773c96670873d95d8e439637d42a9cd6d72fc1
LU <rojaslucia.h@gmail.com>
2021-04-26 19:54:29 -0500

    se colocó el header como componente

--- my-app/src/components/orders-to-deliver.jsx
+++ my-app/src/components/orders-to-deliver.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './header'
 
 const OrdersToDeliver = () => {
     return (
-        <div>
+        <div className="container">
+            <Header />
             <h1>Orders to deliver</h1>
         </div>
     )
