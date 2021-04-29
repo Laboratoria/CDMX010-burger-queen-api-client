5fcbb1454ecd5825b9926cfeb643e072e136c922
Jael91 <jaelorea@gmail.com>
2021-04-29 00:17:42 -0500

    main router for waiter

--- burgerqueen/src/App.js
+++ burgerqueen/src/App.js
@@ -1,37 +1,14 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
   import logo from './assets/logo-burgerqueen.png';
+  import './App.css';
  // import Menu from './Slice/menu';
   
   export default function App() {
@@ -52,7 +29,6 @@ import {
       </Router>
     );
   }
-  
   
   const Login = () => {
     return <div className='login'>
@@ -105,67 +81,37 @@ import {
     }
   
 const Menu = () => {
-    return <div className="div-menu">
-        <div id="logo"><img src={logo} className="logo" alt="logo" /></div>
+  let match = useRouteMatch();
+    return  <Router>
+    <div className="div-button-menu">
+    <div id="logo"><img src={logo} className="App-logo" alt="logo" /></div>
         <div className="button-waiter">
-            <button className="button-orders active">
-                <Link to="/">New Order</Link>
-            </button>
-            <button className="button-orders">
-                <Link to="/pendingOrder">Pending Order</Link>
-            </button>
-            <button className="button-orders">
-                <Link to="/doneOrder">Done Order</Link>
-            </button>
+              <button className="button-orders">
+                <Link to={`${match.url}/`}>New Order</Link> 
+              </button>
+              <button className="button-orders">
+                <Link to={`${match.url}/pendingOrder`}>Pending Order</Link> 
+              </button>
+              <button className="button-orders">
+                <Link to={`${match.url}/doneOrder`}>Done Order</Link> 
+              </button>
         </div>
+      <Switch>      
+        <Route path={`${match.path}/:doneOrder`}> 
+          <Users />
+        </Route>
+        <Route path={`${match.path}/:pendingOrder`}> 
+          <About />
+        </Route>
+        <Route path={match.path}>
+          <Home />
+        </Route>
+      </Switch>
     </div>
+  </Router>
 }
 
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import logo from './assets/logo-burgerqueen.png';
-import './App.css';
-
-export default function App() {
-  return (
-    <Router>
-      <div className="div-button-menu">
-      <div id="logo"><img src={logo} className="App-logo" alt="logo" /></div>
-          <div className="button-waiter">
-                <button className="button-orders">
-                  <Link to="/">New Order</Link>
-                </button>
-                <button className="button-orders">
-                  <Link to="/pendingOrder">Pending Order</Link>
-                </button>
-                <button className="button-orders">
-                  <Link to="/doneOrder">Done Order</Link>
-                </button>
-         </div>
-        <Switch>
-          <Route path="/pendingOrder">
-            <About />
-          </Route>
-          <Route path="/doneOrder">
-            <Users />
-          </Route>
-          {/* <Route path="/MenuAllDay">
-            <MenuAllDay />
-          </Route> */}
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
+// NEW ORDER
 function Home() {
   return <div>
     <p>CLIENT_045</p>
@@ -184,82 +130,13 @@ function Home() {
     <p className="total">Total: $</p>
     <div className="box-send">
       <button className="button-send">
-                <Link to="/doneOrder">ENVIAR</Link>
+        <Link to="/doneOrder">ENVIAR</Link>
       </button>
     </div>
   </div>
 }
 
-// function MenuAllDay() {
-//   return <div>
-//     <p>CLIENT_045</p>
-//     <div className="main-box">
-//       <div className="box-one">
-//         <Link to="/morningOrder"><button>Morning</button></Link>
-//         <Link to="/allOrder"><button>All day ✔</button></Link>
-//       </div>
-//       <div className="box-two">
-//         <p>$10  Hamburguesa simple</p>
-//         <p>$15  Hamburguesa doble</p>
-//         <p>$5 Papas fritas</p>
-//         <p>$5 Aros de cebolla</p>
-//         {/* <p>$5 Agua 500ml</p>
-//         <p>$7 Agua 750ml</p> */}
-//       </div>
-//     </div>
-//     <p className="total">Total: $</p>
-//     <div className="box-send">
-//       <button className="button-send">
-//                 <Link to="/doneOrder">ENVIAR</Link>
-//       </button>
-//     </div>
-//   </div>
-// }
-
-function About() {
-  return <div>
-    {/* <p>CLIENT_047</p> */}
-    <div className="main-box-pending">
-      <div className="box-one-pending">
-        <h3>Morning</h3>
-        <h4>CLIENT-045</h4>
-        <p>$5  Café americano</p>
-        <p>$7  Café con leche</p>
-        <p>$10 Sándwich jamón y queso</p>
-        <p>$7 Jugo de frutas</p>
-      </div>
-      <div className="box-two-pending">
-        <h3>All day</h3>
-        <h4>CLIENT-046</h4>
-        <p>$5  Café americano</p>
-        <p>$7  Café con leche</p>
-        <p>$10 Sándwich jamón y queso</p>
-        <p>$7 Jugo de frutas</p>
-      </div>
-    </div>
-    <div className="second-box-pending">
-      <div className="box-three-pending">
-        <h3>All day</h3>
-        <h4>CLIENT-047</h4>
-        <p>$5  Café americano</p>
-        <p>$7  Café con leche</p>
-        <p>$10 Sándwich jamón y queso</p>
-        <p>$7 Jugo de frutas</p>
-      </div>
-      <div className="box-three-pending">
-        <h3>Morning</h3>
-        <h4>CLIENT-048</h4>
-        <p>$5  Café americano</p>
-        <p>$7  Café con leche</p>
-        <p>$10 Sándwich jamón y queso</p>
-        <p>$7 Jugo de frutas</p>
-      </div>
-    </div>
-    <div className="margin-bottom">
-    </div>
-  </div>
-}
-
+// DONE ORDER
 function Users() {
   return <div>
   {/* <p>CLIENT_047</p> */}
@@ -307,3 +184,172 @@ function Users() {
   </div>
 </div>
 }
+
+// PENDING ORDER
+function About() {
+  return <div>
+    {/* <p>CLIENT_047</p> */}
+    <div className="main-box-pending">
+      <div className="box-one-pending">
+        <h3>Morning</h3>
+        <h4>CLIENT-045</h4>
+        <p>$5  Café americano</p>
+        <p>$7  Café con leche</p>
+        <p>$10 Sándwich jamón y queso</p>
+        <p>$7 Jugo de frutas</p>
+      </div>
+      <div className="box-two-pending">
+        <h3>All day</h3>
+        <h4>CLIENT-046</h4>
+        <p>$5  Café americano</p>
+        <p>$7  Café con leche</p>
+        <p>$10 Sándwich jamón y queso</p>
+        <p>$7 Jugo de frutas</p>
+      </div>
+    </div>
+    <div className="second-box-pending">
+      <div className="box-three-pending">
+        <h3>All day</h3>
+        <h4>CLIENT-047</h4>
+        <p>$5  Café americano</p>
+        <p>$7  Café con leche</p>
+        <p>$10 Sándwich jamón y queso</p>
+        <p>$7 Jugo de frutas</p>
+      </div>
+      <div className="box-three-pending">
+        <h3>Morning</h3>
+        <h4>CLIENT-048</h4>
+        <p>$5  Café americano</p>
+        <p>$7  Café con leche</p>
+        <p>$10 Sándwich jamón y queso</p>
+        <p>$7 Jugo de frutas</p>
+      </div>
+    </div>
+    <div className="margin-bottom">
+    </div>
+  </div>
+}
+
+
+
+    // <div className="div-menu">
+    //     <div id="logo"><img src={logo} className="logo" alt="logo" /></div>
+    //     <div className="button-waiter">
+    //         <button className="button-orders active">
+    //             <Link to="/">New Order</Link>
+    //         </button>
+    //         <button className="button-orders">
+    //             <Link to="/pendingOrder">Pending Order</Link>
+    //         </button>
+    //         <button className="button-orders">
+    //             <Link to="/doneOrder">Done Order</Link>
+    //         </button>
+    //     </div>
+    // </div>
+
+
+// function MenuAllDay() {
+//   return <div>
+//     <p>CLIENT_045</p>
+//     <div className="main-box">
+//       <div className="box-one">
+//         <Link to="/morningOrder"><button>Morning</button></Link>
+//         <Link to="/allOrder"><button>All day ✔</button></Link>
+//       </div>
+//       <div className="box-two">
+//         <p>$10  Hamburguesa simple</p>
+//         <p>$15  Hamburguesa doble</p>
+//         <p>$5 Papas fritas</p>
+//         <p>$5 Aros de cebolla</p>
+//         {/* <p>$5 Agua 500ml</p>
+//         <p>$7 Agua 750ml</p> */}
+//       </div>
+//     </div>
+//     <p className="total">Total: $</p>
+//     <div className="box-send">
+//       <button className="button-send">
+//                 <Link to="/doneOrder">ENVIAR</Link>
+//       </button>
+//     </div>
+//   </div>
+// }
+
+
+
+// export default function App() {
+//   return (
+    // <Router>
+    //   <div className="div-button-menu">
+    //   <div id="logo"><img src={logo} className="App-logo" alt="logo" /></div>
+    //       <div className="button-waiter">
+    //             <button className="button-orders">
+    //               <Link to="/">New Order</Link>
+    //             </button>
+    //             <button className="button-orders">
+    //               <Link to="/pendingOrder">Pending Order</Link>
+    //             </button>
+    //             <button className="button-orders">
+    //               <Link to="/doneOrder">Done Order</Link>
+    //             </button>
+    //      </div>
+    //     <Switch>
+    //       <Route path="/pendingOrder">
+    //         <About />
+    //       </Route>
+    //       <Route path="/doneOrder">
+    //         <Users />
+    //       </Route>
+    //       {/* <Route path="/MenuAllDay">
+    //         <MenuAllDay />
+    //       </Route> */}
+    //       <Route path="/">
+    //         <Home />
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+//   );
+// }
+
+
+
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+// import logo from './assets/logo-burgerqueen.png';
+// import './App.css';
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
