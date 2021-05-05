a44d61d4fe3ee39673d76df46887eeb4eb1eeb47
Jael91 <jaelorea@gmail.com>
2021-05-04 23:08:03 -0500

    json file

--- burgerqueen/src/components/Order/Order.js
+++ burgerqueen/src/components/Order/Order.js
@@ -7,17 +7,17 @@ const Order = ({pending, done}) => {
         <div>
             <div className="main-box-pending">
                 <div className="box-pending-order">
-                    {pending &&
-                        <p>Pending</p>
-                    }
-                    <h3>Morning</h3>
+                    <h3>MORNING</h3>
                     <h4>CLIENT-048</h4>
                     <p>$5  Café americano</p>
                     <p>$7  Café con leche</p>
                     <p>$10 Sándwich jamón y queso</p>
                     <p>$7 Jugo de frutas</p>
+                    {pending &&
+                        <p><strong>PENDING</strong></p>
+                    }
                     {done &&
-                        <p>Done</p>
+                        <p>✔</p>
                     }
                 </div>
                 <div id="rootOrder"></div>
