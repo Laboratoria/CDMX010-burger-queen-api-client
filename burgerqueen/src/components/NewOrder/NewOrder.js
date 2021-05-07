972d287424df31f04356ea1c106607e55d28fb10
MiriamNM <miriamnavamucino@gmail.com>
2021-05-07 14:10:51 -0500

    adding lunch and breakfast states

--- burgerqueen/src/components/NewOrder/NewOrder.js
+++ burgerqueen/src/components/NewOrder/NewOrder.js
@@ -1,23 +1,40 @@
 import React from "react";
+import { useState } from 'react';
 import {
-  BrowserRouter as Router,
-  Link,
+  BrowserRouter as Link,
 } from "react-router-dom";
-import OrdersClient from "../stateApi/stateApi"
+import ApiBreakfast from "../apiBreakfast/ApiBreakfast"
+import ApiLunch from "../apiLunch/ApiLunch"
 import './NewOrder.css';
 
 const NewOrder = () => {
+  const [Breakfast, setBreakfast] = useState(true); 
+  const [Lunch, setLunch] = useState(true); 
+
+  const handlerBreakfast = () => {
+    setBreakfast(!Breakfast);
+  };
+
+  const handlerLunch = () => {
+    setLunch(!Lunch);
+  };
+
   return (
     <div>
       <p id="client-name">CLIENT_045</p>
       <div className="main-box">
          <div className="box-menus">
-          <Link to="/morningOrder"><p>Morning</p></Link>
-          <Link to="/allOrder"><p>All day</p></Link>
+          <button className='button-box-menu' onClick={handlerBreakfast}><p>Morning</p></button>
+          <button className='button-box-menu' onClick={handlerLunch}><p>All day</p></button>
          </div>
          <div className="select-food-products">
            <div className="products-line">
-            <OrdersClient />
+            { (Breakfast === true) &&
+              <ApiBreakfast />
+            }
+            { (Lunch === true) &&
+              <ApiLunch />
+            } 
           </div>
          </div>
       </div>
@@ -27,9 +44,11 @@ const NewOrder = () => {
             <Link to="/doneOrder">ENVIAR</Link>
          </button>
       </div>
+      
+      {/* { (Breakfast === 'breakfast') &&
+          <Order done={done}/>
+      } */}
     </div>
 )
 };
 export default NewOrder;
-
-{/* <Link to="/morningOrder"><Link to="/allOrder"> */}
--- burgerqueen/src/components/apiBreakfast/ApiBreakfast.js
+++ burgerqueen/src/components/apiBreakfast/ApiBreakfast.js
@@ -1,9 +1,9 @@
 import React,{useState, useEffect} from "react";
 //import Orders from "../Order/Order"
 
-//import '../Orders/Orders.css';
+import './apiBreakfast.css';
 
-const OrdersClient =()=>{
+const ApiBreakfast =()=>{
     const url = 'http://localhost:5000/breakfast'
     const [breakfast, setBreakfast] = useState([]);
     const fetchApi = async() => {
@@ -19,19 +19,19 @@ const OrdersClient =()=>{
         fetchApi()
     },[])
     return(
-        <div>
-                {
-                    breakfast.map((items) => {
-                        return<div className='products-line'>
-                            <button className="button-products">${items.price} {items.item}</button>
-                            <button className="button-product-signal">-</button><p>1</p>
-                        </div>
-                    })
-                }
+        <div className="select-food-products-2">
+            {
+                breakfast.map((items) => {
+                    return<div className='products-line-2'>
+                        <button className="button-products-2">${items.price} {items.item}</button>
+                        <button className="button-product-signal-2">-</button><p>1</p>
+                    </div>
+                })
+            }
         </div>
     )
 }
 
-export default OrdersClient;
+export default ApiBreakfast;
 
 //className="select-food-products"className="margin-bottom"
