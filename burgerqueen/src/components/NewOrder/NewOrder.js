d7dd26beb33c86f9a6cea791a5b1d3d1d5a3f395
Jael91 <jaelorea@gmail.com>
2021-05-12 00:45:36 -0500

    adding css responsive

--- burgerqueen/src/components/NewOrder/NewOrder.js
+++ burgerqueen/src/components/NewOrder/NewOrder.js
@@ -3,14 +3,15 @@ import { useState } from 'react';
 import {
   BrowserRouter as Link,
 } from "react-router-dom";
-import ApiBreakfast from "../apiBreakfast/ApiBreakfast"
-import ApiLunch from "../apiLunch/ApiLunch"
+import ApiBreakfast from "../apiBreakfast/ApiBreakfast";
+import ApiLunch from "../apiLunch/ApiLunch";
 import ObjectOrderLunch from "../crudAPI/CrudApiLunch";
+
 import './NewOrder.css';
 
 const NewOrder = (props) => {
   const [Breakfast, setBreakfast] = useState(true); 
-  const [Lunch, setLunch] = useState(true); 
+  const [Lunch, setLunch] = useState(false); 
 
   const handlerBreakfast = () => {
     setBreakfast(!Breakfast);
@@ -22,18 +23,18 @@ const NewOrder = (props) => {
 
   return (
     <div>
-      <div className="clientName"><label htmlFor="name">Name:</label></div>
+      <div className="clientName"><label htmlFor="name">Name: Rosa Carranza</label></div>
       <input type="name" className="inputClient" />
       <div className="main-box">
          <div className="box-menus">
           <button className='button-box-menu' onClick={handlerBreakfast}><p>Morning</p></button>
-          <button className='button-box-menu' onClick={handlerLunch}><p>All day</p></button>
+          <button className='button-box-menu' id='button-box-menu' onClick={handlerLunch}><p>All day</p></button>
             <div id="total">
               <label htmlFor="value" className="totalLabel">Total: </label>
               <input type="value" className="totalInput"></input>
             </div>
          </div>
-         <div className="select-food-products">
+         {/* <div className="select-food-products"> */}
            <div className="products-line">
             { (Breakfast === true) &&
               <ApiBreakfast breakfast={props.breakfast}/>
@@ -42,7 +43,7 @@ const NewOrder = (props) => {
               <ApiLunch lunch={props.lunch}/>
             } 
           </div>
-         </div>
+         {/* </div> */}
       </div>
       <div className="box-send-waiter">
          <button type ="submit" className="button-send-waiter" onClick={ObjectOrderLunch}>
